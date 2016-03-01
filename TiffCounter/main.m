9c20ac82f162fc2d557eb266f29242642b4df8f9
Matt Shepherd <matt.c.shepherd@gmail.com>
2016-03-01 11:46:41 -0800

    Initial Commit

---
+++ TiffCounter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TiffCounter
+//
+//  Created by Matt Shepherd on 3/1/16.
+//  Copyright © 2016 Matt Shepherd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
